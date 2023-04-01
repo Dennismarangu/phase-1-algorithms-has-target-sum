46d45df054a713be04d029774ae8c3a93076680f
+ <+>
2023-04-01 10:54:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,15 +4,41 @@ function hasTargetSum(array, target) {
 
 /* 
   Write the Big O time complexity of your function here
-*/
+
+  The time complexity of the function is O(n^2) since it has nested loops. 
+  It means that the function's performance may decrease as the input size grows. 
+  An optimized solution would be to use a hash table to store the complement of each element in the array.
+  It would reduce the time complexity to O(n). 
+  However, since the problem doesn't specify any constraints, the current solution is sufficient.
+  
+  */
 
 /* 
   Add your pseudocode here
+
+##Create a function called "hasTargetSum" that takes an array and a target integer as arguments.
+##Loop through the array, starting from the first element.
+##Within the first loop, create a second loop starting from the second element.
+##Within the second loop, check if the sum of the two elements is equal to the target integer.
+##If the sum is equal to the target integer, return true.
+##If no pair of elements sums up to the target integer, return false.
+
 */
 
 /*
   Add written explanation of your solution here
 */
+
+function hasTargetSum(arr, target) {
+for (let i = 0; i < arr.length; i++) {
+for (let j = i + 1; j < arr.length; j++) {
+if (arr[i] + arr[j] === target) {
+return true;
+}
+}
+}
+return false;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
